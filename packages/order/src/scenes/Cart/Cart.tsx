c1078ae83d6fad05f412ea5e98031a58dbbc216b
David Nicholas <Dnicholas@bitovi.com>
2024-07-18 09:35:11 -0700

    empty cart

--- packages/order/src/scenes/Cart/Cart.tsx
+++ packages/order/src/scenes/Cart/Cart.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react";
 
-import { Flex, HoverCard } from "@mantine/core";
+import { Flex, HoverCard, Text } from "@mantine/core";
 import { IconShoppingCart } from "@tabler/icons-react";
 
 import { toCurrency } from "@utilities/currency";
@@ -11,16 +11,22 @@ import { useCart } from "./hook/useCart";
 const Cart: FC = () => {
   const cart = useCart();
 
+  const cartHasItems = cart.products.length === 0;
+
   return (
     <HoverCard>
       <HoverCard.Target>
         <Flex c="white">
           <IconShoppingCart color="white" />
-          {cart.products.length === 0 ? null : toCurrency(cart.subTotal)}
+          {cartHasItems ? null : toCurrency(cart.subTotal)}
         </Flex>
       </HoverCard.Target>
       <HoverCard.Dropdown c="black">
-        <CartSummary products={cart.products} />
+        {cartHasItems ? (
+          <CartSummary products={cart.products} />
+        ) : (
+          <Text>No items in cart</Text>
+        )}
       </HoverCard.Dropdown>
     </HoverCard>
   );
