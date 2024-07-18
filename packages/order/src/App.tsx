bbee592c0143e80523ad4cbcf1daba6afaa2a99b
David Nicholas <Dnicholas@bitovi.com>
2024-07-18 09:32:09 -0700

    update landing page for order

--- packages/order/src/App.tsx
+++ packages/order/src/App.tsx
@@ -1,10 +1,11 @@
+import { Button, Container, MantineProvider, Menu } from "@mantine/core";
+import { emotionTransform, MantineEmotionProvider } from "@mantine/emotion";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
 import Cart from "./scenes/Cart";
 import Checkout from "./scenes/Checkout";
 import Shipping from "./scenes/Shipping";
-import { MantineProvider } from "@mantine/core";
-
-import { emotionTransform, MantineEmotionProvider } from "@mantine/emotion";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "@mantine/core/styles.css";
 
@@ -15,9 +16,38 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <MantineProvider stylesTransform={emotionTransform}>
         <MantineEmotionProvider>
-          <Cart />
-          <Checkout />
-          <Shipping onSubmit={() => {}} />
+          <Container fluid>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Checkout />} />
+                <Route
+                  path="/shipping"
+                  element={<Shipping onSubmit={console.log} />}
+                />
+                <Route path="/cart" element={<Cart />} />
+              </Routes>
+              <Menu>
+                <Menu.Target>
+                  <Button
+                    sx={{ position: "absolute", right: "32px", bottom: "32px" }}
+                  >
+                    Other Remotes
+                  </Button>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Item component={Link} to="/shipping">
+                    Shipping
+                  </Menu.Item>
+                  <Menu.Item component={Link} to="/cart">
+                    Cart
+                  </Menu.Item>
+                  <Menu.Item component={Link} to="/">
+                    Checkout
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </BrowserRouter>
+          </Container>
         </MantineEmotionProvider>
       </MantineProvider>
     </QueryClientProvider>
