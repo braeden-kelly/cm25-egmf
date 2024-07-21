6d2075d46bf4bae01a77a7e78c89476872660fd4
David Nicholas <Dnicholas@bitovi.com>
2024-07-20 19:33:47 -0700

    remove css

--- packages/profile/src/scenes/Login.tsx
+++ packages/profile/src/scenes/Login.tsx
@@ -1,6 +1,5 @@
 import type { FC } from "react";
 
-import "../Login.css";
 import backgroundImage from "../../assets/pexels-gustavo-fring-6050428.jpg";
 import {
   Button,
@@ -13,25 +12,26 @@ import {
   TextInput,
 } from "@mantine/core";
 
-export interface Props {
+import { createStyles } from "@mantine/emotion";
+
+export interface LoginProps {
   onLoginSuccess: () => void;
 }
 
-const Login: FC<Props> = ({ onLoginSuccess }) => {
+const Login: FC<LoginProps> = ({ onLoginSuccess }) => {
+  const { classes } = useStyles();
+
   return (
-    <Container className="login-page">
-      <div
-        className="background"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      />
-      <div className="background-screen" />
+    <Container className={classes.loginPage}>
+      <div className={classes.background} />
+      <div className={classes.backgroundScreen} />
 
       <Flex
         direction="column"
         wrap="nowrap"
         align="center"
         justify="center"
-        className="login-centered"
+        className={classes.loginCentered}
       >
         <form
           onSubmit={(e) => {
@@ -53,3 +53,32 @@ const Login: FC<Props> = ({ onLoginSuccess }) => {
 };
 
 export default Login;
+
+const useStyles = createStyles((theme) => ({
+  background: {
+    color: "#ffffff33",
+    position: "fixed",
+    height: "100%",
+    width: "100%",
+    left: 0,
+    top: 0,
+    backgroundSize: "100% auto",
+    backgroundClip: "border-box",
+    zIndex: -2,
+    backgroundImage: `url(${backgroundImage})`,
+  },
+  backgroundScreen: {
+    backgroundColor: "#ffffffdd",
+    zIndex: -1,
+    position: "fixed",
+    height: "100%",
+    width: "100%",
+    left: 0,
+    top: 0,
+    content: "'\\00A0'", // Adding escaped Unicode space character
+  },
+  loginCentered: {
+    zIndex: 10,
+    height: "100vh",
+  },
+}));
