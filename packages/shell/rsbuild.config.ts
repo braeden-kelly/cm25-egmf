ef0f95fabf1c0b6f402731a9207da6c24cd5e80f
David Nicholas <Dnicholas@bitovi.com>
2024-07-22 15:43:36 -0700

    log build

--- packages/shell/rsbuild.config.ts
+++ packages/shell/rsbuild.config.ts
@@ -1,6 +1,53 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 
+console.log({
+  moduleFederation: {
+    options: {
+      name: "shell-application",
+      remotes: {
+        catalog: `catalog@${process.env.MFE_URL_CATALOG}/remoteEntry.js`,
+        order: `order@${process.env.MFE_URL_ORDER}/remoteEntry.js`,
+        profile: `profile@${process.env.MFE_URL_PROFILE}/remoteEntry.js`,
+        marketing: `marketing@${process.env.MFE_URL_MARKETING}/remoteEntry.js`,
+        workshop: `workshop@${process.env.MFE_URL_WORKSHOP}/workshop.js`,
+      },
+      shared: {
+        "@mantine/core": {
+          requiredVersion: "^7.10.2",
+          singleton: true,
+        },
+        "@mantine/emotion": {
+          requiredVersion: false,
+          singleton: true,
+        },
+        "@mantine/notifications": {
+          requiredVersion: false,
+          singleton: true,
+        },
+        "react-router-dom": {
+          requiredVersion: "^6.23.1",
+          singleton: true,
+        },
+        react: {
+          requiredVersion: "^18.2.0",
+          singleton: true,
+          eager: true,
+        },
+        "react-dom": {
+          requiredVersion: "^18.2.0",
+          singleton: true,
+          eager: true,
+        },
+        "@tanstack/react-query": {
+          requiredVersion: "^5.48.0",
+          singleton: true,
+        },
+      },
+    },
+  },
+});
+
 export default defineConfig({
   output: {
     sourceMap: {
