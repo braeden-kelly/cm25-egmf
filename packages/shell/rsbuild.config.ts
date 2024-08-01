c802fc3f7f87e2dc5d474dbc7ce06f1ca7f74910
David Nicholas <Dnicholas@bitovi.com>
2024-07-31 18:07:08 -0700

    update html tags

--- packages/shell/rsbuild.config.ts
+++ packages/shell/rsbuild.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 
+import { tags } from "./tags";
+
 export default defineConfig({
   output: {
     sourceMap: {
@@ -15,36 +17,7 @@ export default defineConfig({
   html: {
     favicon: "./src/assets/favicon.png",
     title: "Enterprise Grade Micro Frontends",
-    tags: [
-      {
-        tag: "base",
-        attrs: { href: "/" },
-        append: false,
-      },
-      {
-        tag: "meta",
-        attrs: {
-          property: "og:title",
-          content: "Enterprise Grade Micro Frontends",
-        },
-      },
-      {
-        tag: "meta",
-        attrs: {
-          property: "og:description",
-          content:
-            "Dive into the world of Micro Frontends in this dynamic 8-hour workshop designed for intermediate and advanced developers. Through a mix of theory and extensive hands-on practice, including a comprehensive e-commerce project, participants will explore how to integrate various frameworks like Angular and React within a Micro Frontend architecture. This workshop will not only clarify the complexities and challenges of Micro Frontends but also equip you with practical strategies for overcoming them. Expect to emerge with a solid grasp of Micro Frontend principles, and ready to implement scalable and flexible solutions that enhance development workflows and product quality. This workshop is perfect for developers keen on mastering advanced architectural patterns to address sophisticated business and team requirements.",
-        },
-      },
-      {
-        tag: "meta",
-        attrs: {
-          property: "og:image",
-          content:
-            "https://davidnic11.github.io/workshop-enterprise-grade-micro-frontends/title-card.png",
-        },
-      },
-    ],
+    tags,
   },
   plugins: [pluginReact()],
   moduleFederation: {
